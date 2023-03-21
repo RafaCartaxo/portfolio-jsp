b4e5b455b3386632247f92aadf881968ca7ac217
Rafael Cartaxo <Rafael.cartaxo@hotmail.com>
2023-03-21 15:34:38 -0300

    Mantendo os dados em tela

--- portfolio-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ portfolio-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -37,8 +37,10 @@ public class ServletUsuarioController extends HttpServlet {
 		modelLogin.setSenha(senha);
 		
 		
-		RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
-		redireciona.forward(request, response);
+		request.setAttribute("modelLogin", modelLogin);
+
+		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+		
 		
 		
 	
